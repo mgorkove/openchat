020754884418245432dd80f33a43d4b9a5ed7925
LeeHoseop <ghtjq3909@naver.com>
2021-03-05 14:55:53 +0900

    change cpu

--- demo/web_demo_env.py
+++ demo/web_demo_env.py
@@ -25,7 +25,7 @@ class WebDemoEnv(BaseEnv):
         def index():
             return render_template("index.html", title=model.name)
 
-        @self.app.route('/send/<user_id>/<text>', methods=['GET'])
+        @self.app.route('/send/<user_id>/<text>', methods=['POST'])
         def send(user_id, text: str) -> Dict[str, str]:
 
             if text in self.keywords:
