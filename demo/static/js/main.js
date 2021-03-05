4c55ffe2659a81116d134a58dfd6efc90b1bfdb1
LeeHoseop <ghtjq3909@naver.com>
2021-03-05 17:54:12 +0900

    Try

--- demo/static/js/main.js
+++ demo/static/js/main.js
@@ -58,7 +58,7 @@ function greet() {
 
 function requestChat(userId, messageText) {
     $.ajax({
-        url: "http://0.0.0.0:8800/send/" + userId + "/" + messageText,
+        url: "/send/" + userId + "/" + messageText,
         type: "GET",
         dataType: "json",
         success: function (data) {
