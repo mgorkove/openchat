a96947a80d0afcc744c8deefacdb67ca58d85f00
LeeHoseop <ghtjq3909@naver.com>
2021-03-05 17:28:13 +0900

    Change GPU

--- demo/start_demo.py
+++ demo/start_demo.py
@@ -1,4 +1,4 @@
 from openchat import OpenChat
 from web_demo_env import WebDemoEnv
 
-OpenChat(model="blenderbot", size="large", env=WebDemoEnv())
+OpenChat(model="blenderbot", size="large", env=WebDemoEnv(), device="cuda")
