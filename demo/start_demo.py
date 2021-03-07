40b1e250b5190aa24914611af6ba9b8052beca62
LeeHoseop <ghtjq3909@naver.com>
2021-03-07 19:29:13 +0900

    Did it

--- demo/start_demo.py
+++ demo/start_demo.py
@@ -2,6 +2,8 @@ from openchat import OpenChat
 from web_demo_env import WebDemoEnv
 
 # CPU Running
-OpenChat(model="blenderbot", size="large", env=WebDemoEnv())
+#print('cpu')
+#OpenChat(model="blenderbot", size="large", env=WebDemoEnv())
 # GPU Running
-#OpenChat(model="blenderbot", size="large", env=WebDemoEnv(), device="cuda")
+print('gpu')
+OpenChat(model="blenderbot", size="large", env=WebDemoEnv(), device="cuda")
