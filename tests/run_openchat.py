fe5067afc0e9ee33a18cfabf2a98ae8eadf93aec
LeeHoseop <ghtjq3909@naver.com>
2021-04-21 10:40:50 +0900

    Fork new version and update web demo env

---
+++ tests/run_openchat.py
@@ -0,0 +1,5 @@
+from openchat import OpenChat
+
+if __name__ == '__main__':
+    OpenChat(model="gptneo.medium", device="cpu", environment='webserver')
+    #OpenChat(model="gptneo.medium", device="cpu")
