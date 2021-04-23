c01adaf822313aeaebc4b9980bce4e36ad2410f0
LeeHoseop <ghtjq3909@naver.com>
2021-04-23 14:39:41 +0900

    Fix

--- tests/run_openchat.py
+++ tests/run_openchat.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
     OpenChats(models=["blender.small", 'dialogpt.medium', 'gptneo.large'],
-              device='gpu',
+              device='cuda',
               environment='webserver',
               method="top_k",
               top_k=25,
