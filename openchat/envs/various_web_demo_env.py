a9c067d7b740141c580c03586aa07de2bb6c0809
Lee Hoseop <ghtjq3909@naver.com>
2021-05-12 19:00:31 +0900

    Change port to 8000

--- openchat/envs/various_web_demo_env.py
+++ openchat/envs/various_web_demo_env.py
@@ -230,7 +230,7 @@ class VariousWebServerEnvironment(BaseEnvironment):
         #from waitress import serve
         #serve(self.app, host='0.0.0.0', port=80)
         #serve(self.app, host='0.0.0.0', port=8000)
-        self.app.run(host='0.0.0.0', port=80)
+        self.app.run(host='0.0.0.0', port=8000)
 
 
     def pre_dialog_for_special_tasks(self, agent, user_id, bot_id, topic):
